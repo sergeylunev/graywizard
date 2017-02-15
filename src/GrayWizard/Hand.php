271c99c7d339fa6de76fea1da05a1586595fb976
Sergey Lunev <sergey@lunyov.ru>
2017-02-16 00:38:55 +0300

    Updating DocBlocks

--- src/GrayWizard/Hand.php
+++ src/GrayWizard/Hand.php
@@ -13,21 +13,36 @@ class Hand
      */
     protected $cards = [];
 
+    /**
+     * @param CardInterface[] $cards Array of instance of Cards
+     */
     public function __construct(array $cards = [])
     {
         $this->cards = $cards;
     }
 
+    /**
+     * Count cards in hand
+     * @return int
+     */
     public function count()
     {
         return count($this->cards);
     }
 
+    /**
+     * Getting max size of hand
+     * @return int 
+     */
     public function getMaximumSize()
     {
         return self::MAX_SIZE;
     }
 
+    /**
+     * Add card to hand
+     * @param CardInterface $card
+     */
     public function addCard(CardInterface $card)
     {
         if ($this->count() === self::MAX_SIZE) {
@@ -37,6 +52,10 @@ class Hand
         $this->cards[] = $card;
     }
 
+    /**
+     * Get list of all cards in hand
+     * @return CardInterface[]
+     */
     public function getCards()
     {
         return $this->cards;
