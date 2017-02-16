d34c19d9bace55b97d4aeda8e4d66c3051fa4f44
ShadowOfElf <abel02@mail.ru>
2017-02-16 13:20:30 +0300

    tab fix

--- src/GrayWizard/Deck.php
+++ src/GrayWizard/Deck.php
@@ -9,22 +9,22 @@ class Deck
     public function __construct($Card)
     {
 		
-		$tempArr = array_count_values($Card);
-		$commonCard = ['Test', 'Coin', 'LightningBolt'];
-		$legendCard = ['PatchesThePirate', 'Leroy'];
-		
-		foreach ($tempArr as $key=>$cardCount)
-		{
-			if (!class_exists('GrayWizard\\Cards\\' . $key . 'Card')) {		
-			    throw new \Exception('Wrong Card: ' . $key);
-		    }		
-			
-		$newClass = ('\GrayWizard\\Cards\\' . $key . 'Card');
-		$newCard = new $newClass();
-			if (($cardCount > 2) or (($cardCount>1)	and ($newCard->isRare() == 'rare'))) {
-				throw new \Exception('Too many copies '.$key);
-			}			
-		}
+        $tempArr = array_count_values($Card);
+        $commonCard = ['Test', 'Coin', 'LightningBolt'];
+        $legendCard = ['PatchesThePirate', 'Leroy'];
+    
+        foreach ($tempArr as $key=>$cardCount)
+        {
+            if (!class_exists('GrayWizard\\Cards\\' . $key . 'Card')) {		
+                throw new \Exception('Wrong Card: ' . $key);
+            }		
+
+        $newClass = ('\GrayWizard\\Cards\\' . $key . 'Card');
+        $newCard = new $newClass();
+            if (($cardCount > 2) or (($cardCount>1)	and ($newCard->isRare() == 'rare'))) {
+                throw new \Exception('Too many copies '.$key);
+            }			
+        }
 		
         if ($Card != []) {
             $this->DeckArray = $Card; 
@@ -35,11 +35,11 @@ class Deck
     {
         return 0;
     }
-	
+    
     public function draw()
     {
-		$newClass = ('\GrayWizard\\Cards\\' . $this->DeckArray[0] . 'Card');
-		$drowCard = new $newClass();
+        $newClass = ('\GrayWizard\\Cards\\' . $this->DeckArray[0] . 'Card');
+        $drowCard = new $newClass();
         unset($this->DeckArray[0]);
         $this->DeckArray = array_values($this->DeckArray);
         return $drowCard;
