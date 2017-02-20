06763fb45cc998c3576b65c6687254b0bda3e858
ShadowOfElf <abel02@mail.ru>
2017-02-20 17:05:50 +0300

    Aya Blackpaw test

---
+++ spec/GrayWizard/Cards/AyaBlackpawCardSpec.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace spec\GrayWizard\Cards;
+
+use GrayWizard\Cards\AyaBlackpawCard;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class AyaBlackpawCardSpec extends ObjectBehavior
+{
+    
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(AyaBlackpawCard::class);
+    }
+    
+    public function it_should_return_this_card_name()
+    {
+        $this->getType()->shouldBeString();
+        $this->getName()->shouldBe('Aya Blackpaw');
+    }
+
+    public function it_should_retunr_type()
+    {
+        $this->getType()->shouldBeString();
+        $this->getType()->shouldBe(CardInterface::TYPE_CREATURE);
+    }
+    
+    public function it_should_have_manacost()
+    {
+        $this->getManaCost()->shouldBeInt();
+        $this->getManaCost()->shouldBe(6);
+    }
+    
+    public function it_should_have_rarity()
+    {
+        $this->getRarity()->shouldBeString();
+        $this->getRarity()->shouldBe(CardInterface::RARITY_RARE);
+    }
+    
+    public function it_should_be_common_card()
+    {
+        $this->isRare()->shouldBeBoolean();
+        $this->isRare()->shouldBe(true);
+    }
+}
