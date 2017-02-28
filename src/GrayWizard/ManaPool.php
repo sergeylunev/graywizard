edcfa29e34e4bb5b561f71f9d44bfa55d30d4b47
Sergey Lunev <sergey@lunyov.ru>
2017-02-28 18:47:47 +0300

    Adding dockBlocks

--- src/GrayWizard/ManaPool.php
+++ src/GrayWizard/ManaPool.php
@@ -6,20 +6,37 @@ class ManaPool
 {
     const MAX_AMOUNT = 10;
 
+    /**
+     * @var int
+     */
     protected $activeMana;
+    /**
+     * @var int
+     */
     protected $manaCrystals;
 
+    /**
+     * ManaPool constructor.
+     */
     public function __construct()
     {
         $this->activeMana = 1;
         $this->manaCrystals = 1;
     }
 
+    /**
+     * @return int
+     */
     public function getActiveMana()
     {
         return $this->activeMana;
     }
 
+    /**
+     * @param int $amount
+     *
+     * @throws \Exception
+     */
     public function spend($amount)
     {
         if (!$this->canSpend($amount)) {
@@ -28,32 +45,22 @@ class ManaPool
         $this->activeMana = $this->activeMana - $amount;
     }
 
+    /**
+     * @param int $amount
+     *
+     * @return bool
+     */
     public function canSpend($amount)
     {
         return $this->activeMana >= $amount;
     }
 
+    /**
+     * @return int
+     */
     public function getManaCrystals()
     {
         return $this->manaCrystals;
-    }
-
-    public function addManaCrystal()
-    {
-        if (!$this->canAddManaCrystal()) {
-            throw new \Exception("We can't add more than maximum amount of mana crystals");
-        }
-        $this->manaCrystals++;
-    }
-
-    public function canAddManaCrystal()
-    {
-        return $this->manaCrystals !== self::MAX_AMOUNT;
-    }
-
-    public function refresh()
-    {
-        $this->activeMana = $this->manaCrystals;
     }
 
     public function onTurnStart()
@@ -62,6 +69,31 @@ class ManaPool
             $this->addManaCrystal();
         }
         $this->refresh();
+    }
+
+    /**
+     * @return bool
+     */
+    public function canAddManaCrystal()
+    {
+        return $this->manaCrystals !== self::MAX_AMOUNT;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function addManaCrystal()
+    {
+        if (!$this->canAddManaCrystal()) {
+            throw new \Exception("We can't add more than maximum amount of mana crystals");
+        }
+
+        $this->manaCrystals++;
+    }
+
+    public function refresh()
+    {
+        $this->activeMana = $this->manaCrystals;
     }
 
     public function addActiveMana()
