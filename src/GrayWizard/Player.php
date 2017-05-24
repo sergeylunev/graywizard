a0358dff0399d09260bb88ac83b84c33381da0a7
ShadowOfElf <abel02@mail.ru>
2017-05-24 20:34:50 +0300

    do player Health, wounds, heal and Alaive check

--- src/GrayWizard/Player.php
+++ src/GrayWizard/Player.php
@@ -5,9 +5,13 @@ namespace GrayWizard;
 use GrayWizard\Interfaces\DeckInterface;
 use GrayWizard\Interfaces\GraveYardInterface;
 use GrayWizard\Interfaces\HandInterface;
+use GrayWizard\Interfaces\PlayerInterface;
 
 class Player
 {
+    
+    public $playerHealth = PlayerInterface::MAX_HEALTH;
+    
     /**
      * @var DeckInterface
      */
@@ -45,21 +49,39 @@ class Player
 
     public function getHealth()
     {
-        // TODO: write logic here
+        // TODO: write logic here        
+        return $this->playerHealth;
     }
 
     public function wounds($argument1)
     {
         // TODO: write logic here
+        $this->playerHealth = $this->playerHealth - $argument1;
+        return $this->playerHealth;
     }
 
     public function heal($argument1)
     {
         // TODO: write logic here
+        if ($this->playerHealth < 30)
+        {
+            $this->playerHealth = $this->playerHealth + $argument1;
+            if ($this->playerHealth > 30)
+            {
+                $this->playerHealth = 30;
+            }
+        }
     }
 
     public function isAlive()
     {
         // TODO: write logic here
+        $playerAlive = true;
+        
+        if ($this->playerHealth <= 0)
+        {
+            $playerAlive = false;
+        }
+        return $playerAlive;
     }
 }
