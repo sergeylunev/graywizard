7e247b95b374b694916a97e14e1c3b4a87b1d318
Sergey Lunev <sergey@lunyov.ru>
2021-01-02 17:44:40 +0300

    Small fixes for the deck

--- src/GrayWizard/Deck.php
+++ src/GrayWizard/Deck.php
@@ -2,49 +2,48 @@
 
 namespace GrayWizard;
 
+use GrayWizard\Interfaces\CardInterface;
 use GrayWizard\Interfaces\DeckInterface;
 
 class Deck implements DeckInterface
 {
-    public $DeckArray = [];
+    public $deckArray = [];
 
     public function __construct(array $card)
-    {	
+    {
         $tempArr = array_count_values($card);
-    
-        foreach ($tempArr as $key => $cardCount) {
-            $cardClass = ('\GrayWizard\\Cards\\' . $key . 'Card');               
-            if (!class_exists($cardClass)) {		
-                throw new \Exception('Wrong Card: ' . $key);
-            }		
-            $newCard = new $cardClass(); 
-        
-            if (($cardCount > 2) || (($cardCount>1)	&& ($newCard->isRare()))) {
-                throw new \Exception('Too many copies '.$key);
-            }			
-        }
-		
-        if ($card != []) {
-            $this->DeckArray = $card; 
-        }
-    }	
 
-    public function shuffle()
-    {
-        return 0;
+        foreach ($tempArr as $key => $cardCount) {
+            $cardClass = ('\GrayWizard\\Cards\\' . $key . 'Card');
+            if (!class_exists($cardClass)) {
+                throw new \Exception('Wrong Card: ' . $key);
+            }
+            $newCard = new $cardClass();
+
+            if (($cardCount > 2) || (($cardCount > 1) && ($newCard->isRare()))) {
+                throw new \Exception('Too many copies ' . $key);
+            }
+        }
+
+        if ($card != []) {
+            $this->deckArray = $card;
+        }
     }
-    
-    public function draw()
+
+    public function shuffle(): void
     {
-        $newClass = ('\GrayWizard\\Cards\\' . $this->DeckArray[0] . 'Card');
-        $drowCard = new $newClass();
-        unset($this->DeckArray[0]);
-        $this->DeckArray = array_values($this->DeckArray);
-        return $drowCard;
+        shuffle($this->deckArray);
+    }
+
+    public function draw(): CardInterface
+    {
+        $newClass = '\GrayWizard\\Cards\\' . array_pop($this->deckArray) . 'Card';
+
+        return new $newClass();
     }
 
     public function count()
     {
-        return count($this->DeckArray);
-    }	
+        return count($this->deckArray);
+    }
 }
